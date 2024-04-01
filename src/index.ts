68afa8f3f38b6db0f5afe46b180050311e1b74ab
Zejammydodger <66030363+Zejammydodger@users.noreply.github.com>
2024-04-01 12:18:54 +0100

    config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import ScoreSaberAPI from "scoresaber.js";
 import express from 'express';
-import { calcPpBoundary, diffToTopX, getPPDifference } from "./utils";
-
+import { calcPpBoundary, diffToTopX, getPPDifference, CONFIG } from "./utils";
 
 const app = express();
 const port = 8081;
 var cors = require('cors')
 var ejs = require("ejs")
 var fs = require("fs")
+
 
 app.use(cors())
 app.set('view engine', 'ejs');
@@ -16,23 +16,36 @@ app.listen(port, () => console.log(`Server listening on port: ${port}`));
 
 app.get('/toPlayer', async function(req, res) {
 	var P2;
-	if (req.query.PlayerSSid != undefined) {
-		P2 = await ScoreSaberAPI.fetchBasicPlayer(req.query.PlayerSSid.toString())
+	if (req.query.targetSSid != undefined) {
+		P2 = await ScoreSaberAPI.fetchBasicPlayer(req.query.targetSSid.toString())
 	} else {res.send("Bad url error"); return;}
-	var data = {"SSid" : req.query.SSid , "PlayerSSid" : req.query.PlayerSSid , "P2name" : P2.name};
-	fs.readFile("../html/overlayToPlayer.html" , "utf-8" , (err : string , html : string) => {
+	console.log(CONFIG.SERVER)
+	var data = {
+		"SSid" : req.query.SSid,
+		"targetSSid" : req.query.targetSSid,
+		"P2name" : P2.name,
+		"SERVER" : CONFIG.SERVER
+	};
+	fs.readFile("./html/overlayToPlayer.html" , "utf-8" , (err : string , html : string) => {
 		res.send(ejs.render(html , data))
 	})
 });
 
 app.get('/toNum', function(req, res) {
-	var data = {"SSid" : req.query.SSid , "num" : req.query.num};
+	var data = {
+		"SSid" : req.query.SSid, 
+		"num" : req.query.num,
+		"SERVER" : CONFIG.SERVER
+	};
 	fs.readFile("../html/overlayToNum.html" , "utf-8" , (err : any , html : any) => {
 		res.send(ejs.render(html , data))
 	})
 });
 app.get('/plusOne', function(req, res) {
-	var data = {"SSid" : req.query.SSid};
+	var data = {
+		"SSid" : req.query.SSid,
+		"SERVER" : CONFIG.SERVER
+	};
 	fs.readFile("../html/overlayPlus1PP.html" , "utf-8" , (err : any , html : any) => {
 		res.send(ejs.render(html , data))
 	})
@@ -44,7 +57,7 @@ app.get('/ppToNum', async (req :any, res: { send: (arg0: string) => void; }) => 
 });
 
 app.get('/ppToPlayer', async (req: any, res: { send: (arg0: string) => void; }) => {
-    res.send((await getPPDifference(req.query.SSid, req.query.PlayerSSid)).toString());
+    res.send((await getPPDifference(req.query.SSid, req.query.targetSSid)).toString());
 });
 
 app.get('/plusOnePP' ,  async (req: any, res: { send: (arg0: string) => void; }) => {
